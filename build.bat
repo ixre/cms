c93eebb29804aa2c045c0eb684689a3091476a75
sonven <new.min@msn.com>
2015-03-28 00:08:37 +0800

    refrence dll

--- build.bat
+++ build.bat
@@ -16,14 +16,13 @@ if exist "%megdir%ILMerge.exe" (
 
 echo  /keyfile:%dir%\Source_Code\Spc.Core\ops.cms.snk>nul
 
-"%megdir%ILMerge.exe" /ndebug /targetplatform:v4 /target:dll /out:%dir%dist\ops.cms.dll^
+"%megdir%ILMerge.exe" /closed /ndebug /targetplatform:v4 /target:dll /out:%dir%dist\ops.cms.dll^
  Ops.Cms.Core.dll Spc.BLL.dll Spc.DAL.dll Ops.Cms.Domain.Interface.dll^
  Ops.Cms.CacheService.dll Ops.Cms.DataTransfer.dll Ops.Cms.Domain.Implement.Content.dll^
- Ops.Cms.DB.dll Ops.Cms.Cache.dll^
- Ops.Cms.Domain.Implement.Site.dll Ops.Cms.Infrastructure.dll Ops.Cms.Service.dll Ops.Cms.ServiceContract.dll^
+ Ops.Cms.DB.dll Ops.Cms.Cache.dll Ops.Cms.Domain.Implement.Site.dll Ops.Cms.Infrastructure.dll ^
+ Ops.Cms.Service.dll Ops.Cms.ServiceContract.dll^
  Ops.Cms.ServiceUtil.dll Ops.Cms.ServiceRepository.dll Spc.IDAL.dll^
- Spc.Sql.dll Ops.Cms.Utility.dll StructureMap.dll Ops.Cms.Web.dll opscore.dll 
-
+ Spc.Sql.dll Ops.Cms.Utility.dll StructureMap.dll Ops.Cms.Web.dll
 
 
   echo 完成!输出到:%dir%dist\ops.cms.dll
