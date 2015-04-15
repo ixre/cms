bb48eff949d7cbb79a46a346792fe55b579371fd
sonven <new.min@msn.com>
2015-04-16 00:06:51 +0800

    commit

--- build.bat
+++ build.bat
@@ -2,30 +2,30 @@
 color 66
 
 echo =======================================
-echo = Ops Cms .NET ! 核心程序集生成工具 =
+echo = AtNet Cms .NET ! 核心程序集生成工具 =
 echo =======================================
 
 
 set dir=%~dp0
 set megdir=%dir%\refrence.dll\
 
-if exist "%megdir%ILMerge.exe" (
+if exist "%megdir%merge.exe" (
 
   echo 生成中,请稍等...
   cd %dir%bin\
 
-echo  /keyfile:%dir%\Source_Code\Spc.Core\ops.cms.snk>nul
+echo  /keyfile:%dir%\Source_Code\AtNet.Cms.Core\atnet.cms.snk>nul
 
-"%megdir%ILMerge.exe" /closed /ndebug /targetplatform:v4 /target:dll /out:%dir%dist\ops.cms.dll^
- Ops.Cms.Core.dll Spc.BLL.dll Spc.DAL.dll Ops.Cms.Domain.Interface.dll^
- Ops.Cms.CacheService.dll Ops.Cms.DataTransfer.dll Ops.Cms.Domain.Implement.Content.dll^
- Ops.Cms.DB.dll Ops.Cms.Cache.dll Ops.Cms.Domain.Implement.Site.dll Ops.Cms.Infrastructure.dll ^
- Ops.Cms.Service.dll Ops.Cms.ServiceContract.dll^
- Ops.Cms.ServiceUtil.dll Ops.Cms.ServiceRepository.dll Spc.IDAL.dll^
- Spc.Sql.dll Ops.Cms.Utility.dll StructureMap.dll Ops.Cms.Web.dll
+"%megdir%merge.exe" /closed /ndebug /targetplatform:v4 /target:dll /out:%dir%dist\atnet.cms.dll^
+ AtNet.Cms.Core.dll AtNet.Cms.BLL.dll AtNet.Cms.DAL.dll AtNet.Cms.Domain.Interface.dll^
+ AtNet.Cms.CacheService.dll AtNet.Cms.DataTransfer.dll AtNet.Cms.Domain.Implement.Content.dll^
+ AtNet.Cms.DB.dll AtNet.Cms.Cache.dll AtNet.Cms.Domain.Implement.Site.dll AtNet.Cms.Infrastructure.dll ^
+ AtNet.Cms.Service.dll AtNet.Cms.ServiceContract.dll^
+ AtNet.Cms.ServiceUtil.dll AtNet.Cms.ServiceRepository.dll AtNet.Cms.IDAL.dll^
+ AtNet.Cms.Sql.dll AtNet.Cms.Utility.dll StructureMap.dll AtNet.Cms.Web.dll
 
 
-  echo 完成!输出到:%dir%dist\ops.cms.dll
+  echo 完成!输出到:%dir%dist\atnet.cms.dll
 
 )
 
