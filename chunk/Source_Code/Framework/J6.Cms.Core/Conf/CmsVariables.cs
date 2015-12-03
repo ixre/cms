7224bc7a48614224a04ca7e9a8fc4808ef27e82d
jarrysix <jarrysix@gmail.com>
2015-12-03 17:35:48 +0800

    commit

--- chunk/Source_Code/Framework/J6.Cms.Core/Conf/CmsVariables.cs
+++ chunk/Source_Code/Framework/J6.Cms.Core/Conf/CmsVariables.cs
@@ -6,7 +6,7 @@ namespace J6.Cms.Conf
         /// <summary>
     	/// 版本号
     	/// </summary>
-        public const string VERSION="1.2.35";
+        public const string VERSION="1.2.36";
 
         /// <summary>
         /// 框架目录
