21b56cab44fe141ddf11b8d7f492f98d60e61f7c
jarry6 <jarrysix@gmail.com>
2016-04-18 01:06:51 +0800

    refactor

--- src/cms/core/J6.Cms.Core/Conf/Server.cs
+++ src/cms/core/J6.Cms.Core/Conf/Server.cs
@@ -16,7 +16,7 @@ namespace J6.Cms.Conf
     	/// 默认静态服务器
     	/// 
     	/// </summary>
-        internal static string DefaultStaticServer="ssp-sz.z3q.net/cms";
+        internal static string DefaultStaticServer="cdn.cms.k3f.net/cms";
 
 
         /// <summary>
@@ -27,6 +27,6 @@ namespace J6.Cms.Conf
         /// <summary>
         /// 更新服务器
         /// </summary>
-        public static string DefaultUpgradeServer = "http://ssp-sz.z3q.net/cms/patch/latest/";
+        public static string DefaultUpgradeServer = "http://up.cms.k3f.net/cms/patch/latest/";
     }
 }
