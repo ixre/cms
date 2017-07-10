3c68b09fa13020f73288067072128a0f4cfc5415
jack lei <79289908@qq.com>
2017-07-10 11:58:46 +0000

    UploadC.cs 已在 Bitbucket 中在线编辑过

--- src/cms/core/JR.Cms.Web/WebManager/Handle/UploadC.cs
+++ src/cms/core/JR.Cms.Web/WebManager/Handle/UploadC.cs
@@ -27,16 +27,25 @@ namespace JR.Cms.Web.WebManager.Handle
         /// </summary>
         public void UploadImage_POST()
         {
-            string uploadfor = base.Request["for"];
-            string id = base.Request["upload.id"];
-            DateTime dt = DateTime.Now;
-            string dir = string.Format("/{0}s{1}/image/{2:yyyyMM}/", CmsVariables.RESOURCE_PATH, base.CurrentSite.SiteId.ToString(), dt);
-            string name = String.Format("{0}{1:ddHHss}{2}",
-                String.IsNullOrEmpty(uploadfor) ? "" : uploadfor + "_",
-                dt, String.Empty.RandomLetters(4));
+            //远程上传
+            System.Web.HttpPostedFile postfile = Request.Files[0];
+            string strOutput = string.Empty;
+            System.Net.CookieContainer cookie = new System.Net.CookieContainer();
+            HttpPostFile("http://img.0xa.com/upload/index", postfile, null, null, ref strOutput);
 
-            string file = new FileUpload(dir, name).Upload();
-            Response.Write("{" + String.Format("url:'{0}'", file) + "}");
+            JsonData JD = JsonMapper.ToObject(strOutput);
+            Response.Write("{" + String.Format("url:'{0}'", "http://img.0xa.com"+JD["raw"]) + "}");
+            
+            //string uploadfor = base.Request["for"];
+            //string id = base.Request["upload.id"];
+            //DateTime dt = DateTime.Now;
+            //string dir = string.Format("/{0}s{1}/image/{2:yyyyMM}/", CmsVariables.RESOURCE_PATH, base.CurrentSite.SiteId.ToString(), dt);
+            //string name = String.Format("{0}{1:ddHHss}{2}",
+                //String.IsNullOrEmpty(uploadfor) ? "" : uploadfor + "_",
+                //dt, String.Empty.RandomLetters(4));
+
+            //string file = new FileUpload(dir, name).Upload();
+            //Response.Write("{" + String.Format("url:'{0}'", file) + "}");
         }
 
         public void UploadFile_POST()
