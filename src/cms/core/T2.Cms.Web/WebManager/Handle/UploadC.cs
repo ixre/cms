e443fc490ee90e3901e3358714764653ea01f8e2
刘铭 <jarrysix@gmail.com>
2018-07-29 02:02:29 +0800

    Reset UploadC.cs

--- src/cms/core/T2.Cms.Web/WebManager/Handle/UploadC.cs
+++ src/cms/core/T2.Cms.Web/WebManager/Handle/UploadC.cs
@@ -16,7 +16,6 @@ using T2.Cms.Conf;
 using T2.Cms.WebManager;
 using JR.DevFw.Framework.Extensions;
 using JR.DevFw.Framework.Web.UI;
-using LitJson;
 using System.Net;
 using System.Collections.Generic;
 using System.IO;
@@ -32,24 +31,24 @@ namespace T2.Cms.Web.WebManager.Handle
         public void UploadImage_POST()
         {
             //远程上传
-            System.Web.HttpPostedFile postfile = Request.Files[0];
-            string strOutput = string.Empty;
-            System.Net.CookieContainer cookie = new System.Net.CookieContainer();
-            HttpPostFile("http://img.0xa.com/upload/index", postfile, null, null, ref strOutput);
+            //System.Web.HttpPostedFile postfile = Request.Files[0];
+            //string strOutput = string.Empty;
+            //System.Net.CookieContainer cookie = new System.Net.CookieContainer();
+            //HttpPostFile("http://img.0xa.com/upload/index", postfile, null, null, ref strOutput);
 
-            JsonData JD = JsonMapper.ToObject(strOutput);
-            Response.Write("{" + String.Format("url:'{0}'", "http://img.0xa.com"+JD["raw"]) + "}");
+            //JsonData JD = JsonMapper.ToObject(strOutput);
+            //Response.Write("{" + String.Format("url:'{0}'", "http://img.0xa.com"+JD["raw"]) + "}");
             
-            //string uploadfor = base.Request["for"];
-            //string id = base.Request["upload.id"];
-            //DateTime dt = DateTime.Now;
-            //string dir = string.Format("/{0}s{1}/image/{2:yyyyMM}/", CmsVariables.RESOURCE_PATH, base.CurrentSite.SiteId.ToString(), dt);
-            //string name = String.Format("{0}{1:ddHHss}{2}",
-                //String.IsNullOrEmpty(uploadfor) ? "" : uploadfor + "_",
-                //dt, String.Empty.RandomLetters(4));
+           string uploadfor = base.Request["for"];
+            string id = base.Request["upload.id"];
+            DateTime dt = DateTime.Now;
+            string dir = string.Format("/{0}s{1}/image/{2:yyyyMM}/", CmsVariables.RESOURCE_PATH, base.CurrentSite.SiteId.ToString(), dt);
+            string name = String.Format("{0}{1:ddHHss}{2}",
+                String.IsNullOrEmpty(uploadfor) ? "" : uploadfor + "_",
+                dt, String.Empty.RandomLetters(4));
 
-            //string file = new FileUpload(dir, name).Upload();
-            //Response.Write("{" + String.Format("url:'{0}'", file) + "}");
+            string file = new FileUpload(dir, name).Upload();
+            Response.Write("{" + String.Format("url:'{0}'", file) + "}");
         }
         
         #region 文件上传至远程服务器
