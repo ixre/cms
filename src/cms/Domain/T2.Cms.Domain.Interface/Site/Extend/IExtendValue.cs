ac67a98aae2b0f7a15dbed2176949b9f5d1c1dac
刘铭 <jarrysix@gmail.com>
2018-08-17 15:30:36 +0800

    project structure

--- src/cms/Domain/T2.Cms.Domain.Interface/Site/Extend/IExtendValue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace T2.Cms.Domain.Interface.Site.Extend
-{
-    public interface IExtendValue:IDomain<int>
-    {
-        IExtendField Field { get; }
-        string Value { get; set; }
-    }
-}
--- src/cms/UIApp/RunningToolkit/Scripts/com/db.py
+++
@@ -1,6 +0,0 @@
-﻿import clr
-clr.AddReference("j6.cms");
-from J6.Data import DataBaseAccess
-
-def read(sql):
-	dba = DataBaseAccess()
