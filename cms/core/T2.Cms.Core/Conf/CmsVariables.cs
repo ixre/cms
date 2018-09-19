3a725c528c7ad2671823f90e4e262c0681417c10
刘铭 <jarrysix@gmail.com>
2018-09-19 20:12:43 +0800

    JS更新和code mirror

--- cms/core/T2.Cms.Core/Conf/CmsVariables.cs
+++ cms/core/T2.Cms.Core/Conf/CmsVariables.cs
@@ -6,7 +6,7 @@ namespace T2.Cms.Conf
         /// <summary>
     	/// 版本号
     	/// </summary>
-        public const string VERSION="3.0.02";
+        public const string VERSION="3.0.03";
 
         /// <summary>
         /// 框架目录
