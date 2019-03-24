ce0ac19691bd822b97e3173bef76e56ec45632af
刘铭 <jarrysix@gmail.com>
2019-03-25 00:25:04 +0800

    base.js

--- src/core/JR.Cms.Core/Conf/CmsVariables.cs
+++ src/core/JR.Cms.Core/Conf/CmsVariables.cs
@@ -11,7 +11,7 @@ namespace JR.Cms.Conf
         /// <summary>
     	/// 版本号
     	/// </summary>
-        public const string VERSION="3.2.018";
+        public const string VERSION="3.2.019";
 
         /// <summary>
         /// 框架目录
