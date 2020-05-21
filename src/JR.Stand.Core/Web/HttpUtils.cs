67d15ddd2a0856bd0e5d63e15dd3b23439bd1174
jarrysix <jarrysix@gmail.com>
2020-05-21 16:52:49 +0800

    Remove Scheme

--- src/JR.Stand.Core/Web/HttpUtils.cs
+++ src/JR.Stand.Core/Web/HttpUtils.cs
@@ -8,12 +8,10 @@ namespace JR.Stand.Core.Web
         
         public static bool IsHttpsRequest(ICompatibleRequest request)
         {
-            //throw new Exception(request.GetHeader("SSL-FLAG")??"xxx" +"|"+request.GetHeader("From-Https")??"OO");
             if (request.GetScheme() == "https") return true;
             if (request.GetHeader("X-Forwarded-Proto") == "https") return true;
             // 兼容西部数码虚拟主机
-            if (request.GetHeader("SSL-FLAG") == "SSL" ||
-                request.GetHeader("From-Https") == "on") return true;
+            if (request.GetHeader("SSL-FLAG") == "SSL" || request.GetHeader("From-Https") == "on") return true;
             return false;
         }
     }
