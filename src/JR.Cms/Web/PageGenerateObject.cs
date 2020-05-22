d310ca0644e6661042bbf97ec27a7fa5ab7c1d0e
jarrysix <jarrysix@gmail.com>
2020-05-23 07:43:01 +0800

    PageArchive

--- src/JR.Cms/Web/PageGenerateObject.cs
+++ src/JR.Cms/Web/PageGenerateObject.cs
@@ -130,7 +130,6 @@ namespace JR.Cms.Web
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="args"></param>
         /// <returns></returns>
         public string GetIndex()
         {
@@ -150,7 +149,6 @@ namespace JR.Cms.Web
         /// </summary>
         /// <param name="category"></param>
         /// <param name="pageIndex"></param>
-        /// <param name="args"></param>
         /// <returns></returns>
         public string GetCategory(CategoryDto category, int pageIndex)
         {
@@ -257,7 +255,7 @@ namespace JR.Cms.Web
 
                     page.AddVariable("page", new PageVariable
                     {
-                        Title = $"{archive.Title}_{category.Name}_{_site.Title}",
+                        Title = $"{archive.Title}_{_site.Title}",
                         SubTitle = _site.Title,
                         Keywords = archive.Tags,
                         Description = pageArchive.Outline.Replace("\"", string.Empty)
