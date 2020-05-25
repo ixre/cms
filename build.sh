0d70542d22f4cad4cccb43d42831fb2d9c5bc531
jarrysix <jarrysix@gmail.com>
2020-05-25 10:33:22 +0800

    build script

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ RELEASE_DIR=$(pwd)/out/release
 rm -rf out && mkdir -p ${RELEASE_DIR} && \
     cd src/JR.Cms.App && dotnet restore && \
     dotnet publish -c Release -o ${RELEASE_DIR} && \
-    cp -r public templates oem install plugins root ${RELEASE_DIR} && \
+    mkdir ${RELEASE_DIR}/root && cp -r root/*.md ${RELEASE_DIR}/root && \
+    mkdir ${RELEASE_DIR}/templates && cp -r templates/default ${RELEASE_DIR}/templates && \
+    cp -r public oem install plugins ${RELEASE_DIR} && \
     cd ${RELEASE_DIR} && \
     rm -rf *.pdb *.xml appsettings.json appsettings.Development.json && \
     cp ../../LICENSE ../../README.md . && \
