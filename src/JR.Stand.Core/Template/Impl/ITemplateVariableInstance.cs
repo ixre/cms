0dfe422c1b7f1087079770f133c69e9072013986
jarrysix <jarrysix@gmail.com>
2021-10-27 15:35:22 +0800

    Fix: {site.data(name)}bug

--- src/JR.Stand.Core/Template/Impl/ITemplateVariableInstance.cs
+++ src/JR.Stand.Core/Template/Impl/ITemplateVariableInstance.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace JR.Stand.Core.Template.Impl
 {
     /// <summary>
-    /// 模板变量对象
+    /// 模板变量对象,实现该接口可使用${var.data("key")}的方式动态获取数据
     /// </summary>
     public interface ITemplateVariableInstance
     {
