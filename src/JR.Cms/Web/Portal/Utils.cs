615ad3df7b10a86a01a2c113d6f106e0cfd75de3
jarrysix <jarrysix@gmail.com>
2021-10-27 19:54:38 +0800

    Fix: https反向代理导致https://后重定向为http://

--- src/JR.Cms/Web/Portal/Utils.cs
+++ src/JR.Cms/Web/Portal/Utils.cs
@@ -20,17 +20,26 @@ namespace JR.Cms.Web.Portal
         public static string GetSiteRedirectUrl(ICompatibleRequest request, SiteDto siteDto)
         {
             if (siteDto.SeoForceHttps == 0 && siteDto.SeoForceRedirect == 0) return null;
+            // 如果本机地址,则不跳转
+            string host = request.GetHost();
+            if (host == "localhost") return null;
+            
             var target = request.GetEncodedUrl();
             if (target == null) return null;
+            // 如果为反向代理HTTPS,需要替换原始http为https
+            string proto = request.GetProto();
+            if (proto == "https" && !target.StartsWith("https"))
+            {
+                target = target.Replace("http://", "https://");
+            }
+            // 当前请求非https,强制启用https
             var forceHttps = false;
-            if (siteDto.SeoForceHttps == 1 && request.GetProto()!="https")
+            if (siteDto.SeoForceHttps == 1 && proto != "https")
             {
                 target = target.Replace("http://", "https://");
                 forceHttps = true;
             }
 
-            string host = request.GetHost();
-            if (host == "localhost") return null;
             var hostParts = host.Split('.').Length;
             // 跳转到带www的二级域名
             if (siteDto.SeoForceRedirect == 1 && hostParts == 2)
