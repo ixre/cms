32ba4901f056e9fad41307f2b98c0bb23c96555f
jarrysix <jarrysix@gmail.com>
2021-11-28 08:42:16 +0800

    Fix: 模板字段长度为零时不返回默认值的BUG

---
+++ src/JR.Stand.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JR.Stand.Core.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 如果字符串为空或长度为零，则返回默认值
+        /// </summary>
+        /// <param name="sourceStream"></param>
+        /// <param name="destination"></param>
+        public static String EmptyElse(this String s, String e)
+        {
+            if (String.IsNullOrEmpty(s)) return e;
+            return s;
+        }
+
+
+    }
+}
