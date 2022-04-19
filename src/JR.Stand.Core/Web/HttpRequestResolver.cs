eaf06754796e6557acfcf5953125b0ae66dfdeaa
jarrysix <jarrysix@gmail.com>
2022-04-19 21:45:48 +0800

    RouteConfig

---
+++ src/JR.Stand.Core/Web/HttpRequestResolver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JR.Stand.Core.Web
+{
+    internal class HttpRequestResolver
+    {
+    }
+}
--- src/NetFx/JR.Cms.AspNet/Mvc/RouteConfig.cs
+++ src/NetFx/JR.Cms.AspNet/Mvc/RouteConfig.cs
@@ -183,6 +183,7 @@ namespace JR.Cms.WebImpl.Mvc
         {
             routes.Clear();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           // RouteDebug.RouteDebugger.RewriteRoutesForTesting(routes);
             routes.MapMvcAttributeRoutes();
             //在加载cms后避免覆盖 /_cms
             RegisterCmsMajorRoutes(routes);
