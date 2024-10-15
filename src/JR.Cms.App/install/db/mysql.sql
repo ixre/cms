804e00cc564efb827da6d0caaebc8a46056aab1a
jarrysix <jarrysix@gmail.com>
2024-10-15 17:58:50 +0800

    fix: #SITE-18 SITE-17 文章内容需兼容emoji表情

--- src/JR.Cms.App/install/db/mysql.sql
+++ src/JR.Cms.App/install/db/mysql.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- ALTER DATABASE cms CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- ALTER TABLE cms_archive CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+--
 --
 -- Table structure for table `cms_archive`
 --
